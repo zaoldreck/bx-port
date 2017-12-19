58a6f6379b77d1c1d68fc15d73130809019be019
Zaoldreck <zaoldreck@gmail.com>
2017-12-19 21:20:13 +0700

    Add temp transaction

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,188 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Transaction.create(
-  date: Time.zone.today,
-  symbol: "OMG",
-  status: "BUY"
-  price: 305,
-  volumn: 99.99,
-  commission: 50
-)
+[
+  {
+    date: "7/10/2017 20:51",
+    symbol: "OMG",
+    status: "BUY",
+    price: 284.61,
+    volumn: 6,
+    commission: 4.27
+  },
+  {
+    date: "7/10/2017 20:51",
+    symbol: "OMG",
+    status: "BUY",
+    price: 284.61,
+    volumn: 29.12506588,
+    commission: 20.77
+  },
+  {
+    date: "13/11/2017 19:33",
+    symbol: "OMG",
+    status: "BUY",
+    price: 254.8,
+    volumn: 19.58127615,
+    commission: 12.5
+  },
+  {
+    date: "13/11/2017 19:48",
+    symbol: "OMG",
+    status: "BUY",
+    price: 256.99,
+    volumn: 12.21464176,
+    commission: 7.86
+  },
+  {
+    date: "13/11/2017 19:48",
+    symbol: "OMG",
+    status: "BUY",
+    price: 257,
+    volumn: 7.20051194,
+    commission: 4.63
+  },
+  {
+    date: "13/11/2017 19:48",
+    symbol: "OMG",
+    status: "SELL",
+    price: 298,
+    volumn: 13.61929614,
+    commission: 10.14
+  },
+  {
+    date: "29/11/2017 23:20",
+    symbol: "OMG",
+    status: "SELL",
+    price: 298,
+    volumn: 26.7080625,
+    commission: 19.89
+  },
+  {
+    date: "29/11/2017 23:30",
+    symbol: "OMG",
+    status: "SELL",
+    price: 298,
+    volumn: 11.07773625,
+    commission: 8.25
+  },
+  {
+    date: "29/11/2017 23:32",
+    symbol: "OMG",
+    status: "SELL",
+    price: 298,
+    volumn: 1.00419463,
+    commission: 0.74
+  },
+  {
+    date: "29/11/2017 23:34",
+    symbol: "OMG",
+    status: "SELL",
+    price: 298,
+    volumn: 7.10663864,
+    commission: 5.29
+  },
+  {
+    date: "5/12/2017 22:01",
+    symbol: "OMG",
+    status: "BUY",
+    price: 338,
+    volumn: 49.875,
+    commission: 42.25
+  },
+  {
+    date: "7/12/2017 19:37",
+    symbol: "BTC",
+    status: "BUY",
+    price: 538900,
+    volumn: 0.01097746,
+    commission: 14.82
+  },
+  {
+    date: "7/12/2017 23:18",
+    symbol: "BTC",
+    status: "BUY",
+    price: 570000,
+    volumn: 0.03509627,
+    commission: 50.13
+  },
+  {
+    date: "13/12/2017 20:21",
+    symbol: "LTC",
+    status: "BUY",
+    price: 10550,
+    volumn: 0.9975,
+    commission: 26.37
+  },
+  {
+    date: "13/12/2017 20:22",
+    symbol: "ETH",
+    status: "BUY",
+    price: 22474,
+    volumn: 0.11297305,
+    commission: 6.36
+  },
+  {
+    date: "13/12/2017 20:22",
+    symbol: "ETH",
+    status: "BUY",
+    price: 22475,
+    volumn: 0.11271186,
+    commission: 6.34
+  },
+  {
+    date: "13/12/2017 20:22",
+    symbol: "ETH",
+    status: "BUY",
+    price: 22475,
+    volumn: 0.00277833,
+    commission: 0.15
+  },
+  {
+    date: "13/12/2017 20:22",
+    symbol: "ETH",
+    status: "BUY",
+    price: 22475,
+    volumn: 0.19508081,
+    commission: 10.98
+  },
+  {
+    date: "13/12/2017 20:22",
+    symbol: "ETH",
+    status: "BUY",
+    price: 22475,
+    volumn: 0.19508081,
+    commission: 10.98
+  },
+  {
+    date: "16/12/2017 23:09",
+    symbol: "ZEC",
+    status: "BUY",
+    price: 0.024,
+    volumn: 0.00941808,
+    commission: 0.00000056
+  },
+  {
+    date: "16/12/2017 23:09",
+    symbol: "ZEC",
+    status: "BUY",
+    price: 0.024,
+    volumn: 0.02005772,
+    commission: 0.00048139
+  },
+  {
+    date: "16/12/2017 23:11",
+    symbol: "ZEC",
+    status: "BUY",
+    price: 0.024,
+    volumn: 0.20056266,
+    commission: 0.00001206
+  },
+  {
+    date: "16/12/2017 23:12",
+    symbol: "ZEC",
+    status: "BUY",
+    price: 0.024,
+    volumn: 0.76746154,
+    commission: 0.00004616
+  }
+].each do |hash|
+  Transaction.create(hash)
+end
